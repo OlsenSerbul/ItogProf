2064e19d785c555828c207571ba3ab41c0a934cc
Olga Serbul <Olsen.zika@mail.ru>
2022-12-07 12:59:56 +0300

    Task_сделали_красиво

--- Itog/Task/Program.cs
+++ Itog/Task/Program.cs
@@ -1,45 +1,44 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
 // длинна которого меньше, либо равна 3 символа.
 // просим пользователя заполнить массив
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+}
 Console.WriteLine("Введите любые 5 слов или символов через Enter:");
 string[] array = new string[5];
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = Console.ReadLine();
 }
-// выводим массив 
+ 
 Console.Write("Массив до сортировки: ");
-for(int i = 0; i < array.Length; i++)
-{
-    Console.Write($"{array[i]}  ");
-}
-// перемешиваем элементы массива, чтобы программа выдала случайные,а не по порядку
+PrintArray(array);
+
 Random rand = new Random();
- 
-    for (int i = array.Length - 1; i >= 0; i--)
-    {
-        int j = rand.Next(i + 1);
- 
-        string temp = array[j];
-        array[j] = array[i];
-        array[i] = temp;
-    }
-Console.WriteLine(); 
-// проверяем 
-for(int i = 0; i < array.Length; i++)
+
+for (int i = array.Length - 1; i >= 0; i--)
 {
-    Console.Write($"{array[i]}  ");
+    int j = rand.Next(i + 1);
+
+    string temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
 }
 Console.WriteLine();
+
 Console.Write("Массив после сортировки: ");
-// метод сортировка элементов со случайным количеством элементов до 3
+
 void SortArray(string[] array)
-{ 
+{
     int N = array.Length;
     N = new Random().Next(1, 4);
-    for(int i = 0; i < N; i++ )
+    for (int i = 0; i < N; i++)
     {
-         Console.Write($"{array[i]}  ");
+        Console.Write($"{array[i]}  ");
     }
 }
 SortArray(array);
