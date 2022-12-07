846e0ef8c4650da2a68996b15a55907a7207ee50
Olga Serbul <Olsen.zika@mail.ru>
2022-12-07 12:36:35 +0300

    Task_еремешали_элементы

--- Itog/Task/Program.cs
+++ Itog/Task/Program.cs
@@ -13,3 +13,21 @@ for(int i = 0; i < array.Length; i++)
 {
     Console.Write($"{array[i]}  ");
 }
+// перемешиваем элементы массива, чтобы программа выдала случайные,а не по порядку
+Random rand = new Random();
+ 
+    for (int i = array.Length - 1; i >= 0; i--)
+    {
+        int j = rand.Next(i + 1);
+ 
+        string temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+    }
+Console.WriteLine(); 
+// проверяем 
+Console.Write("Массив до сортировки: ");
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}  ");
+}
