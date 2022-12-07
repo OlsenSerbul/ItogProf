1ba1f69bd13564069b313be4ccb6888d46c1d13e
Olga Serbul <Olsen.zika@mail.ru>
2022-12-07 12:26:04 +0300

    Task_заполнение_массива

--- Itog/Task/Program.cs
+++ Itog/Task/Program.cs
@@ -1,2 +1,15 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
 // длинна которого меньше, либо равна 3 символа.
+// просим пользователя заполнить массив
+Console.WriteLine("Введите любые 5 слов или символов через Enter:");
+string[] array = new string[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Console.ReadLine();
+}
+// выводим массив 
+Console.Write("Массив до сортировки: ");
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}  ");
+}
